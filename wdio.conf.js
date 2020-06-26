9c2b0306f1e7327c4ce0703dbc021698705a0f54
Luigi <luigi@umbrellavalidator.com>
2020-06-26 16:22:04 -0700

    Edit wdio to sync mode
    So that login.spec.js looks cleaner without the async/await keywords in
    the demo.

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,6 +57,7 @@ exports.config = {
             'goog:chromeOptions': {
                 args: ['--headless', '--disable-gpu'],
             },
+
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
